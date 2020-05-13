b00c1bbefd4aef78d0ca9080581a34ed433ecf72
Dimitar Dimov <dimitar.dimov9204@gmail.com>
2020-05-13 11:50:49 +0100

    applying TS analysis

---
+++ mainAnalysisAndForecasting.py
@@ -0,0 +1,281 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 10:15:26 2020
+
+@author: dimit
+"""
+# Basic packages
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import random as rd # generating random numbers
+import datetime # manipulating date formats
+# Viz
+import matplotlib.pyplot as plt # basic plotting
+import seaborn as sns # for prettier plots
+from sklearn.metrics import median_absolute_error, mean_squared_error, mean_squared_log_error
+
+
+# TIME SERIES
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from pandas.plotting import autocorrelation_plot
+from statsmodels.tsa.stattools import adfuller, acf, pacf,arma_order_select_ic
+import statsmodels.formula.api as smf
+import statsmodels.tsa.api as smt
+import statsmodels.api as sm
+import scipy.stats as scs
+
+
+# settings
+import warnings
+warnings.filterwarnings("ignore")
+
+item_cat=pd.read_csv("item_categories.csv")
+item=pd.read_csv("items.csv")
+sub=pd.read_csv("sample_submission.csv")
+shops=pd.read_csv("shops.csv")
+test=pd.read_csv("test.csv")
+sales = pd.read_csv("sales_train.csv")
+sales.date=sales.date.apply(lambda x:datetime.datetime.strptime(x, '%d.%m.%Y'))
+sales.head(5)
+sales.info()
+
+ts=sales.groupby(["date_block_num"])["item_cnt_day"].sum()
+ts.astype('float')
+
+plt.figure(figsize=(16,8))
+plt.title('Total Sales of the company')
+plt.xlabel('Time')
+plt.ylabel('Sales')
+plt.plot(ts);
+
+plt.figure(figsize=(16,6))
+plt.plot(ts.rolling(window=12,center=False).mean(),label='Rolling Mean');
+plt.plot(ts.rolling(window=12,center=False).std(),label='Rolling sd');
+plt.legend();
+
+def plot_moving_average(series, window, plot_intervals=False, scale=1.96):
+
+    rolling_mean = series.rolling(window=window).mean()
+
+    plt.figure(figsize=(17,8))
+    plt.title('Moving average\n window size = {}'.format(window))
+    plt.plot(rolling_mean, 'g', label='Rolling mean trend')
+
+    #Plot confidence intervals for smoothed values
+    if plot_intervals:
+        mae = mean_absolute_error(series[window:], rolling_mean[window:])
+        deviation = np.std(series[window:] - rolling_mean[window:])
+        lower_bound = rolling_mean - (mae + scale * deviation)
+        upper_bound = rolling_mean + (mae + scale * deviation)
+        plt.plot(upper_bound, 'r--', label='Upper bound / Lower bound')
+        plt.plot(lower_bound, 'r--')
+
+    plt.plot(series[window:], label='Actual values')
+    plt.legend(loc='best')
+    plt.grid(True)
+
+#Smooth by the previous 5 days (by week)
+plot_moving_average(ts, 5)
+
+def exponential_smoothing(series, alpha):
+
+    result = [series[0]] # first value is same as series
+    for n in range(1, len(series)):
+        result.append(alpha * series[n] + (1 - alpha) * result[n-1])
+    return result
+  
+def plot_exponential_smoothing(series, alphas):
+ 
+    plt.figure(figsize=(17, 8))
+    for alpha in alphas:
+        plt.plot(exponential_smoothing(series, alpha), label="Alpha {}".format(alpha))
+    plt.plot(series.values, "c", label = "Actual")
+    plt.legend(loc="best")
+    plt.axis('tight')
+    plt.title("Exponential Smoothing")
+    plt.grid(True);
+
+plot_exponential_smoothing(ts [0.05, 0.3])
+
+def double_exponential_smoothing(series, alpha, beta):
+
+    result = [series[0]]
+    for n in range(1, len(series)+1):
+        if n == 1:
+            level, trend = series[0], series[1] - series[0]
+        if n >= len(series): # forecasting
+            value = result[-1]
+        else:
+            value = series[n]
+        last_level, level = level, alpha * value + (1 - alpha) * (level + trend)
+        trend = beta * (level - last_level) + (1 - beta) * trend
+        result.append(level + trend)
+    return result
+
+def plot_double_exponential_smoothing(series, alphas, betas):
+     
+    plt.figure(figsize=(17, 8))
+    for alpha in alphas:
+        for beta in betas:
+            plt.plot(double_exponential_smoothing(series, alpha, beta), label="Alpha {}, beta {}".format(alpha, beta))
+    plt.plot(series.values, label = "Actual")
+    plt.legend(loc="best")
+    plt.axis('tight')
+    plt.title("Double Exponential Smoothing")
+    plt.grid(True)
+
+plot_double_exponential_smoothing(ts, [0.05, 0.3], [0.1,0.2])
+
+# Let's check that with a quick decomposition into Trend, seasonality and residuals
+
+import statsmodels.api as sm
+
+# multiplicative
+res = sm.tsa.seasonal_decompose(ts.values,freq=12,model="multiplicative")
+plt.figure(figsize=(16,12))
+fig = res.plot()
+fig.show()
+
+# Additive model
+res2 = sm.tsa.seasonal_decompose(ts.values,freq=12,model="additive")
+plt.figure(figsize=(16,12))
+fig = res2.plot()
+fig.show()
+
+# check for stationarity - ADF 
+
+def test_stationarity(timeseries):
+    
+    #Perform Dickey-Fuller test:
+    print('Results of Dickey-Fuller Test:')
+    dftest = adfuller(timeseries, autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print (dfoutput)
+
+test_stationarity(ts)
+
+# p-value of 14%
+
+# remove trend and make data stationary
+
+# to remove trend
+from pandas import Series as Series
+# create a differenced series
+
+def difference(dataset, interval=1):
+    diff = list()
+    for i in range(interval, len(dataset)):
+        value = dataset[i] - dataset[i - interval]
+        diff.append(value)
+    return Series(diff)
+
+# invert differenced forecast
+def inverse_difference(last_ob, value):
+    return value + last_ob
+
+ts=sales.groupby(["date_block_num"])["item_cnt_day"].sum()
+ts.astype('float')
+
+plt.figure(figsize=(16,16))
+plt.subplot(311)
+plt.title('Original')
+plt.xlabel('Time')
+plt.ylabel('Sales')
+plt.plot(ts)
+
+plt.subplot(312)
+plt.title('After De-trend')
+plt.xlabel('Time')
+plt.ylabel('Sales')
+
+new_ts=difference(ts)
+plt.plot(new_ts)
+plt.plot()
+
+plt.subplot(313)
+plt.title('After De-seasonalization')
+plt.xlabel('Time')
+plt.ylabel('Sales')
+
+new_ts=difference(ts,12)       # assuming the seasonality is 12 months long
+plt.plot(new_ts)
+plt.plot()
+
+# we can go back to the original series by using the inverse function
+
+# lets model AR MA and the combination of both ARMA
+
+def tsplot(y, lags=None, figsize=(10, 8), style='bmh',title=''):
+    if not isinstance(y, pd.Series):
+        y = pd.Series(y)
+    with plt.style.context(style):    
+        fig = plt.figure(figsize=figsize)
+        layout = (3, 2)
+        ts_ax = plt.subplot2grid(layout, (0, 0), colspan=2)
+        acf_ax = plt.subplot2grid(layout, (1, 0))
+        pacf_ax = plt.subplot2grid(layout, (1, 1))
+        qq_ax = plt.subplot2grid(layout, (2, 0))
+        pp_ax = plt.subplot2grid(layout, (2, 1))
+        
+        y.plot(ax=ts_ax)
+        ts_ax.set_title(title)
+        smt.graphics.plot_acf(y, lags=lags, ax=acf_ax, alpha=0.5)
+        smt.graphics.plot_pacf(y, lags=lags, ax=pacf_ax, alpha=0.5)
+        sm.qqplot(y, line='s', ax=qq_ax)
+        qq_ax.set_title('QQ Plot')        
+        scs.probplot(y, sparams=(y.mean(), y.std()), plot=pp_ax)
+
+        plt.tight_layout()
+    return 
+
+# simulate AR process with alpha = 0.5
+    
+np.random.seed(1)
+n_samples = int(1000)
+a = 0.5
+x = w = np.random.normal(size=n_samples)
+
+for t in range(n_samples):
+    x[t] = a*x[t-1] + w[t]
+    
+limit=12    
+_ = tsplot(x, lags=limit,title="AR(1)process")
+
+
+# Simulate an MA(1) process
+n = int(1000)
+
+# set the AR(p) alphas equal to 0
+
+alphas = np.array([0.])
+betas = np.array([0.8])
+
+# add zero-lag and negate alphas
+
+ar = np.r_[1, -alphas]
+ma = np.r_[1, betas]
+ma1 = smt.arma_generate_sample(ar=ar, ma=ma, nsample=n) 
+limit=12
+_ = tsplot(ma1, lags=limit,title="MA(1) process")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
